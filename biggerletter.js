69abfbdf04a984a53f6f3c4f9f80c79ba8298fa8
Cassino <renatocassino@gmail.com>
2023-05-01 02:21:07 -0300

    feat: new words

--- biggerletter.js
+++ biggerletter.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 const files = fs.readdirSync("./find_words");
 const sortedFiles = files.sort((a, b) => (a.length > b.length ? -1 : 1));
 
-for (let i = 0; i < 10; i++) {
-  console.log(sortedFiles[i], sortedFiles[i].length, [...Array(sortedFiles[i].length)].map(() => '27').join('*'), '=', Intl.NumberFormat().format(27**sortedFiles[i].length).replace(/,/g, '.'));
+for (let i = 0; i < 30; i++) {
+  console.log(sortedFiles[i], sortedFiles[i].length, [...Array(sortedFiles[i].length)].map(() => '27').join('*'), '=', Intl.NumberFormat('en', { notation: 'compact' }).format(27**sortedFiles[i].length).replace(/,/g, '.'));
 }
 
 console.log('Palavras encontradas: ', files.length);
